a98b7d9e1d1643bf839a6700178ee885a5802af3
VivviV <2108935518@qq.com>
2026-06-09 19:30:48 +0800

    feat: add postgrad-major module API

---
+++ apps/user/src/api/postgrad-major/index.ts
@@ -0,0 +1,17 @@
+import request from '@haifeng/shared/utils/request'
+import type { R, PageResult } from '@haifeng/shared'
+import type { PostgradMajorListVO, PostgradMajorDetailVO, PostgradMajorQueryDTO, PostgradMajorBriefVO, UniversityBriefForPostgradVO } from '@/types/postgrad-major'
+
+const PREFIX = '/api/v1/app/postgrad-major'
+
+export const getPostgradMajorList = (params: PostgradMajorQueryDTO) =>
+  request.get<R<PageResult<PostgradMajorListVO>>>(`${PREFIX}/list`, { params })
+
+export const getPostgradMajorDetail = (majorId: number) =>
+  request.get<R<PostgradMajorDetailVO>>(`${PREFIX}/${majorId}/detail`)
+
+export const getPostgradMajorsByUniversity = (universityId: number, params: { page?: number; size?: number; degreeType?: string }) =>
+  request.get<R<PageResult<PostgradMajorBriefVO>>>(`/api/v1/app/university/${universityId}/postgrad-majors`, { params })
+
+export const getUniversitiesByPostgradMajor = (majorId: number, params: { page?: number; size?: number; category?: string }) =>
+  request.get<R<PageResult<UniversityBriefForPostgradVO>>>(`${PREFIX}/${majorId}/universities`, { params })
