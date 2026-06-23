bb3d2525d06aff0f70f2e6fe8e2d7f9e1a5158de
VivviV <2108935518@qq.com>
2026-06-23 11:31:23 +0800

    feat(user): add job index type definitions

---
+++ apps/user/src/types/employment/jobIndex/index.ts
@@ -0,0 +1,50 @@
+// apps/user/src/types/employment/jobIndex/index.ts
+export interface JobSearchDTO {
+  page?: number
+  size?: number
+  keyword?: string
+  province?: string
+  city?: string
+  educationRequirement?: string
+  recruitmentType?: string
+  salaryMin?: number
+  salaryMax?: number
+  positionStatus?: string
+  categoryLabel?: string
+}
+
+export interface JobIndexListVO {
+  id: number
+  categoryLabel: string
+  positionName: string
+  organizationName: string
+  city: string
+  educationRequirement: string
+  recruitmentType: string
+  salaryText: string
+  positionStatus: string
+}
+
+export interface JobIndexDetailVO {
+  id: number
+  sourceType: string
+  sourceId: number
+  categoryLabel: string
+  positionName: string
+  organizationName: string
+  organizationLogo: string
+  province: string
+  city: string
+  educationRequirement: string
+  recruitmentCount: number
+  recruitmentType: string
+  salaryMin: number
+  salaryMax: number
+  salaryText: string
+  positionStatus: string
+  publishDate: string
+  regDeadline: string
+  isHot: boolean
+  viewCount: number
+  applyCount: number
+}
