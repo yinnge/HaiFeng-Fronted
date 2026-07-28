ce85aeffb7b072725f57c6cd1cbec45f18d17404
VivviV <2108935518@qq.com>
2026-07-28 13:56:54 +0800

    1

--- apps/admin/src/components.d.ts
+++ apps/admin/src/components.d.ts
@@ -9,7 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     BorderBeam: typeof import('./components/ui/BorderBeam.vue')['default']
     DotPattern: typeof import('./components/ui/DotPattern.vue')['default']
-    ElAlert: typeof import('element-plus/es')['ElAlert']
     ElAside: typeof import('element-plus/es')['ElAside']
     ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
